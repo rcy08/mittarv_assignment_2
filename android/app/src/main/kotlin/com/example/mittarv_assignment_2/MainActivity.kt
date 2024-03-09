e2b1e625fa40cd2a3d1512e50e51760a36621a22
Chinmay <rajchinmay08@gmail.com>
2024-03-09 17:58:54 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mittarv_assignment_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mittarv_assignment_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "82b29146ad78d7c6b8d025ee18201fc3";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+
+}
